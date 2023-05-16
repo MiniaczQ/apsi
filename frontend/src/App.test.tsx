dfeef0b583b2aa112fe6a0b680d89db4565457f2
Dove6 <24943032+Dove6@users.noreply.github.com>
2023-05-16 19:22:48 +0200

    Fix the test

--- frontend/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const loginLogoutElement = screen.getByText(/log/i);
-  expect(loginLogoutElement).toBeInTheDocument();
-});

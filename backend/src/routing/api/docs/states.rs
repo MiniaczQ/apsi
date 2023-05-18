5dc5dea7efb08bd95be774bb5e906f95486cca3d
Jakub Motyka <jakub.motyka@daftmobile.com>
2023-05-18 18:12:52 +0200

    feat: most restrictions done

--- backend/src/routing/api/docs/states.rs
+++ backend/src/routing/api/docs/states.rs
@@ -9,27 +9,69 @@ use tracing::error;
 use uuid::Uuid;
 
 use crate::{
-    models::version_state::DocumentVersionState,
+    models::{role::DocumentVersionRole, version_state::DocumentVersionState},
     services::{
         auth::{auth_keys::AuthKeys, claims::Claims},
-        database::{repositories::documents::DocumentsRepository, DbPool},
+        database::{
+            repositories::{documents::DocumentsRepository, permission::PermissionRepository},
+            DbPool,
+        },
+        util::Res2,
     },
 };
 
 async fn change_state(
     documents_repository: DocumentsRepository,
-    _: Claims,
+    permission_repository: PermissionRepository,
+    claims: Claims,
     Path((document_id, version_id, new_state)): Path<(Uuid, Uuid, DocumentVersionState)>,
-) -> StatusCode {
+) -> Res2 {
+    let required_roles = match new_state {
+        DocumentVersionState::InProgress => {
+            vec![DocumentVersionRole::Owner, DocumentVersionRole::Editor]
+        }
+        DocumentVersionState::ReadyForReview => {
+            vec![DocumentVersionRole::Owner, DocumentVersionRole::Editor]
+        }
+        DocumentVersionState::Reviewed => vec![DocumentVersionRole::Reviewer],
+        DocumentVersionState::Published => vec![DocumentVersionRole::Owner, DocumentVersionRole::Publisher],
+    };
+    match permission_repository
+        .does_user_have_document_version_roles(
+            claims.user_id,
+            document_id,
+            version_id,
+            &required_roles,
+        )
+        .await
+    {
+        Ok(true) => {}
+        Ok(false) => {
+            return Res2::Msg((
+                StatusCode::BAD_REQUEST,
+                "User does not have permission to perform this state change",
+            ));
+        }
+        Err(error) => {
+            error!(
+                { error = error.to_string() },
+                "Error when checking permission for changing state"
+            );
+            return Res2::NoMsg(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
     match documents_repository
         .change_state(document_id, version_id, new_state)
         .await
     {
-        Ok(true) => StatusCode::OK,
-        Ok(false) => StatusCode::BAD_REQUEST,
+        Ok(true) => Res2::NoMsg(StatusCode::OK),
+        Ok(false) => Res2::Msg((
+            StatusCode::BAD_REQUEST,
+            "Version could not be updated to desired state from current state",
+        )),
         Err(error) => {
             error!({ error = error.to_string() }, "Error when changing state");
-            StatusCode::INTERNAL_SERVER_ERROR
+            Res2::NoMsg(StatusCode::INTERNAL_SERVER_ERROR)
         }
     }
 }

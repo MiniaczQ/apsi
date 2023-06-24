c6de82b2dc70bb27b08808f1787b1320ec7c2115
buczi <59873761+buczi@users.noreply.github.com>
2023-06-24 15:13:41 +0200

    Manual update Versions.tsx

--- frontend/src/versions/Versions.tsx
+++ frontend/src/versions/Versions.tsx
@@ -13,17 +13,7 @@ type VersionsProps = {
   apiClient: ApiClient
 };
 
-const getFormattedDate = (createdAt: string) => {
-  const date = new Date(createdAt);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = String(date.getFullYear());
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-
-  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
-}
+const getFormattedDate = (createdAt: string) => new Date(createdAt).toLocaleString('ro-RO');
 
 export const Versions: FunctionComponent<VersionsProps> = ({ apiClient }) => {
   const navigate = useNavigate();

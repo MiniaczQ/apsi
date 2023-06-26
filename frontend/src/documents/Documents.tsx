d8741100520b1f439f133a1243fdd9bfffc515cd
buczi <59873761+buczi@users.noreply.github.com>
2023-06-26 23:15:04 +0200

    Add sortable table (#98)
    * Add sortable table
    
    * Ensure unique table keys
    
    * Fix PR comments
    
    * Fix gitattributes
    
    * Remove pngs
    
    * Add pngs
    
    * Delete default
    
    * Add default
    
    * Fix date sorting
    
    * Package update
    
    * simplify aggregation
    
    ---------
    
    Co-authored-by: Maciek <bukalom98@gmail.com>

--- frontend/src/documents/Documents.tsx
+++ frontend/src/documents/Documents.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent, useState, useEffect } from 'react';
-import { Button, Container, Table } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
 
 import ApiClient from '../api/ApiClient';
 import DocumentVersion from '../models/DocumentVersion';
+import { SortedTable } from '../table/SortedTable';
+import { Column } from '../table/TableBody';
 
 
 type DocumentsProps = {
@@ -14,6 +16,14 @@ type DocumentNamedVersion = {
   documentName: String,
   documentVersion: DocumentVersion
 }
+
+const columns = [
+  { label: '#', accessor: 'index', sortable: false, sortByOrder: 'asc', rowSpan: 2},
+  { label: 'Version', accessor: 'version', sortable: true, sortByOrder: 'asc', rowSpan: 2},
+  { label: 'Name', accessor: 'name', sortable: true, sortByOrder: 'asc', colSpan: 2, group: 'Most recent version'},
+  { label: 'Created at', accessor: 'created', sortable: true, sortByOrder: 'asc', colSpan: 2, group: 'Most recent version'}, 
+  { label: 'Options', accessor: 'option', sortable: false, sortByOrder: 'asc', rowSpan: 2}   
+] as Column[]
 
 const getFormattedDate = (createdAt: string) => new Date(createdAt).toLocaleString('ro-RO');
 
@@ -44,7 +54,6 @@ const distinctByDocumentId = (array: DocumentNamedVersion[]) => {
 export const Documents: FunctionComponent<DocumentsProps> = ({ apiClient }) => {
   const navigate = useNavigate();
   const [docsVersions, setDocsVersions] = useState<DocumentNamedVersion[]>([])
-
   useEffect(() => {
     apiClient.getDocuments()
       .then(documentsResponse => {
@@ -52,7 +61,7 @@ export const Documents: FunctionComponent<DocumentsProps> = ({ apiClient }) => {
           document => {
             apiClient.getVersions(document.documentId)
             .then(response => {
-                setDocsVersions(old =>[...old,{documentName: document.documentName, documentVersion: response.slice().sort(compareByCreationTime).at(0)!} ]);
+                setDocsVersions(old => [...old,{documentName: document.documentName, documentVersion: response.slice().sort(compareByCreationTime).at(0)!} ]);
             })
           }
         )
@@ -63,64 +72,24 @@ export const Documents: FunctionComponent<DocumentsProps> = ({ apiClient }) => {
   const navigateToVersionList = (documentId: string) => navigate(`/Versions?documentId=${encodeURIComponent(documentId)}`);
   const navigateToDocumentCreator = () => navigate('/Versions/new');
 
-  const documentRows = distinctByDocumentId(docsVersions).map(({ documentVersion, documentName }: DocumentNamedVersion, index: number) => (
-    <tr key={documentVersion.documentId}>
-      <td>
-        {index + 1}
-      </td>
-      <td align="center">
-        {documentName}
-      </td>
-      <td align='center'>
-        {documentVersion.versionName}
-      </td>
-      <td align='center'>
-        {getFormattedDate(documentVersion.createdAt)}
-      </td>
-      <td align='center'>
-        <Button variant="outline-secondary" onClick={() => navigateToVersionList(documentVersion.documentId)}>
-          Check versions
-        </Button>
-      </td>
-    </tr>
-  ));
-
+  const data = distinctByDocumentId(docsVersions).map(({ documentVersion, documentName }: DocumentNamedVersion, index: number) => ({
+    index: index + 1,
+    version: documentName,
+    name: documentVersion.versionName,
+    created: getFormattedDate(documentVersion.createdAt),
+    option: (
+      <Button variant="outline-secondary" onClick={() => navigateToVersionList(documentVersion.documentId)}>
+        Check versions
+      </Button>
+    )
+  }));
 
   return (
     <Container>
       <h3>
         Documents
       </h3>
-
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th rowSpan={2}>
-              #
-            </th>
-            <th rowSpan={2}>
-              Version
-            </th>
-            <th colSpan={2}>
-              Most recent version
-            </th>
-            <th rowSpan={2}>
-              Options
-            </th>
-          </tr>
-          <tr>
-          <th >
-              Name
-            </th>
-            <th >
-            Created at
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {documentRows}
-        </tbody>
-      </Table>
+      <SortedTable data={data} columns={columns}/>
       <p>
         <Button variant="outline-primary" onClick={navigateToDocumentCreator}>
           Create Document
---
+++ frontend/src/table/SortedTable.tsx
@@ -0,0 +1,84 @@
+import { Table } from "react-bootstrap";
+import { TableBody, Column, SortOrder } from "./TableBody";
+import TableHead from "./TableHead";
+
+import { useEffect, useState } from "react";
+import moment from "moment";
+type SortedTableProps<T> = {
+  data: T[]
+  columns: Column[]
+}
+
+export function SortedTable<T>(props: SortedTableProps<T>) {
+  const useSortableTable = (data: T[], columns: Column[]) => {
+    const handleSorting = (sortField: T, sortOrder: SortOrder) => {
+      if (sortField) {
+        const sorted = [...tableData].sort((a, b) => {
+          if (a[sortField] === null) return 1;
+          if (b[sortField] === null) return -1;
+          if (a[sortField] === null && b[sortField] === null) return 0;
+          if (a[sortField].match(/[0-9]{2}\.[0-9]{2}\.[0-9]{4}, [0-9]{2}:[0-9]{2}:[0-9]{2}/) ||
+            b[sortField].match(/[0-9]{2}\.[0-9]{2}\.[0-9]{4}, [0-9]{2}:[0-9]{2}:[0-9]{2}/)) {
+              const aDate = moment(a[sortField], 'DD.MM.YYYY, HH:mm:ss').toDate()
+              const bDate = moment(b[sortField], 'DD.MM.YYYY, HH:mm:ss').toDate()
+              if(aDate > bDate){
+                return -1 * (sortOrder === "asc" ? 1 : -1);
+              }
+              if(aDate < bDate){
+                return 1 * (sortOrder === "asc" ? 1 : -1);
+              }
+              return 0 * (sortOrder === "asc" ? 1 : -1);
+          }
+          return (
+            a[sortField].toString().localeCompare(b[sortField].toString(), "en", {
+              numeric: true,
+            }) * (sortOrder === "asc" ? 1 : -1)
+          );
+        });
+        setTableData(sorted);
+      }
+    };
+
+    return [tableData, handleSorting];
+  };
+
+  const [tableData, setTableData] = useState<any[]>([]);
+  const [tData, handleSorting] = useSortableTable(props.data, props.columns);
+  useEffect(() => {
+    setTableData(getDefaultSorting(props.data, props.columns))
+  }, [props.data, props.columns])
+
+  return (
+    <>
+      <Table striped bordered hover size="sm">
+        <TableHead columns={props.columns} handleSorting={handleSorting as (sortField: any, sortOrder: SortOrder) => void} />
+        <TableBody columns={props.columns} tableData={tData as any[]} />
+      </Table>
+    </>
+  );
+};
+
+function getDefaultSorting(defaultTableData: any[], columns: Column[]): any[] {
+  const sorted = [...defaultTableData].sort((a, b) => {
+    const filterColumn = columns.filter((column) => column.sortByOrder);
+
+    let { accessor = "id", sortbyOrder = "asc" } = Object.assign(
+      {},
+      ...filterColumn
+    );
+
+    if (a[accessor] === null) return 1;
+    if (b[accessor] === null) return -1;
+    if ((a[accessor] === null) && (b[accessor] === null)) return 0;
+
+    const ascending = a[accessor]
+      .toString()
+      .localeCompare(b[accessor].toString(), "en", {
+        numeric: true,
+      });
+
+    return sortbyOrder === "asc" ? ascending : -ascending;
+  });
+  return sorted;
+}
+

7485a4642d47c8816bbfd34f281e39e665fde70f
Dove6 <24943032+Dove6@users.noreply.github.com>
2023-06-27 20:33:59 +0200

    Fix duplicated versions in document set version
    Handle permission errors

--- frontend/src/set_version_documents/SetVersionDocuments.tsx
+++ frontend/src/set_version_documents/SetVersionDocuments.tsx
@@ -3,11 +3,12 @@ import { Button, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
 
-import ApiClient from '../api/ApiClient';
+import ApiClient, { PermissionError } from '../api/ApiClient';
 import DocumentVersion from '../models/DocumentVersion';
 import Document from '../models/Document';
 import { SortedTable } from '../table/SortedTable';
 import { Column } from '../table/TableBody';
+import DocumentVersionSet from '../models/DocumentVersionSet';
 
 type DocumentsSetProps = {
   apiClient: ApiClient
@@ -18,7 +19,7 @@ const columns = [
   { label: 'Name', accessor: 'name', sortable: true, sortByOrder: 'asc',  rowSpan: 2 },
   { label: 'Version', accessor: 'version', sortable: true, sortByOrder: 'asc', rowSpan: 2 },
   { label: 'Options', accessor: 'option', sortable: false, sortByOrder: 'asc', rowSpan: 2 }
-] as Column[]
+] as Column[];
 
 
 export const SetVersionDocuments: FunctionComponent<DocumentsSetProps> = ({ apiClient }) => {
@@ -27,36 +28,68 @@ export const SetVersionDocuments: FunctionComponent<DocumentsSetProps> = ({ apiC
   const searchParams = useSearchParams()[0];
   const documentSetId = searchParams.get('documentSetId') ?? undefined;
   const versionSetId = searchParams.get('versionSetId') ?? undefined;
-
-  const [docs, setDocs] = useState<Document[]>([])
-  const [vers, setVers] = useState<DocumentVersion[]>([])
+  const gotRequiredSearchParams = documentSetId !== undefined && versionSetId !== undefined;
 
   useEffect(() => {
-    if (documentSetId === undefined || versionSetId === undefined)
+    if (gotRequiredSearchParams)
+      return;
+    if (documentSetId !== undefined)
+      navigate(`/SetVersions?documentSetId=${documentSetId}`);
+    else
+      navigate('/Sets');
+  }, [documentSetId, gotRequiredSearchParams, navigate]);
+
+  const [documentSetVersion, setDocumentSetVersion] = useState<DocumentVersionSet>();
+  const [docs, setDocs] = useState<Document[]>([]);
+  const [vers, setVers] = useState<DocumentVersion[]>([]);
+
+  useEffect(() => {
+    if (!gotRequiredSearchParams)
       return;
     apiClient.getSetVersions(documentSetId)
       .then(response => {
-        response.forEach(
-          setVersion => {
-            if (setVersion.setVersionId === versionSetId) {
-              setVersion.documentVersionIds.forEach(
-                (pair, index) => {
-                  apiClient.getDocument(pair[0]).then(doc => setDocs(oldDocs => [...oldDocs, doc]));
-                  apiClient.getVersion(pair[0], pair[1]).then(ver => setVers(oldVers => [...oldVers, ver]))
-                }
-              )
-            }
-          }
-        )
+        const currentSetVersion = response.find(setVersion => setVersion.setVersionId === versionSetId);
+        if (currentSetVersion === undefined)
+          throw new Error('No such version');
+        setDocumentSetVersion(currentSetVersion);
+      })
+      .catch(() => {
+        if (documentSetId !== undefined)
+          navigate(`/SetVersions?documentSetId=${documentSetId}`);
+        else
+          navigate('/Sets');
       });
-  }, [apiClient, documentSetId, versionSetId]);
+  }, [apiClient, documentSetId, versionSetId, gotRequiredSearchParams, navigate]);
+
+  const filterOutPermissionError = (e: Error) => {
+    if (e instanceof PermissionError)
+      return null;
+    throw e;
+  };
+
+  useEffect(() => {
+    if (documentSetVersion === undefined)
+      return;
+    const promises = documentSetVersion.documentVersionIds.map(([docId, verId]) => [
+      apiClient.getDocument(docId).catch(filterOutPermissionError),
+      apiClient.getVersion(docId, verId).catch(filterOutPermissionError),
+    ]);
+    Promise.all(promises.map(([docPromise, ]) => docPromise as Promise<Document>))
+      .then(setDocs);
+    Promise.all(promises.map(([, verPromise]) => verPromise as Promise<DocumentVersion>))
+      .then(setVers);
+  }, [apiClient, documentSetVersion]);
 
   const data = vers.map((ver: DocumentVersion, index: number) => ({
     index: index + 1,
-    name: typeof docs[index] !== 'undefined' ? docs[index].documentName : '',
-    version: ver.versionName,
-    option: (
+    name: docs[index]?.documentName ?? 'Unknown document',
+    version: ver?.versionName ?? 'Unknown version',
+    option: (docs[index] !== null && ver !== null) ? (
       <Button variant="outline-secondary" onClick={() => navigate(`/DocVer?documentId=${encodeURIComponent(ver.documentId)}&versionId=${encodeURIComponent(ver.versionId)}`)}>
+        Check versions
+      </Button>
+    ) : (
+      <Button disabled variant="outline-danger">
         Check versions
       </Button>
     )
